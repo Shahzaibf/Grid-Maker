3617d7f4bd8bececb7db0a498d962d1827c21974
Shahzaib Fareed <shahzaibfareed1118@gmail.com>
2023-10-08 18:21:55 -0400

    Base code added

---
+++ script.js
@@ -0,0 +1,35 @@
+let numRows = 0;
+let numCols = 0;
+let colorSelected;
+
+function addR() {
+
+}
+
+function addC() {
+
+}
+
+function removeR() {
+
+}
+
+function removeC() {
+
+}
+
+function fillU() {
+
+}
+
+function fillall() {
+
+}
+
+function clearAll() {
+
+}
+
+function selectColor() {
+    
+}
